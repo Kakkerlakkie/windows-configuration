ef4dc5bcebe150995a02b8ac621d69c36dd63ce8
Kakkerlakkie <33314920+Kakkerlakkie@users.noreply.github.com>
2017-11-02 11:52:10 +0100

    Test

---
+++ test.ps1
@@ -0,0 +1,27 @@
+# ============================================================================
+# Name		: Windows Server Post Configuration
+# Author	: Peter van Barneveld  
+# Date		: 01-11-2017
+# Edit		: 01-11-2017
+# Version	: 1.0
+# 
+# Description:
+# 
+#
+# ============================================================================
+#
+# Script tested on: 
+# - Windows Server 2016
+#
+# ============================================================================
+
+
+# Create Default Directories
+# ==========================
+New-Item -type directory -path C:\Temp
+New-Item -type directory -path C:\Xcellent
+New-Item -type directory -path C:\Xcellent\Certificate
+New-Item -type directory -path C:\Xcellent\Log
+New-Item -type directory -path C:\Xcellent\Reports
+New-Item -type directory -path C:\Xcellent\Reports\DocuExport
+New-Item -type directory -path C:\Xcellent\Scripts
--- windows-post-configuration.ps1
+++ windows-post-configuration.ps1
@@ -75,4 +75,4 @@ $HighPerf = powercfg /l | %{if($_.contains("High performance")) {$_.split()[3]}}
 $CurrPlan = $(powercfg /getactivescheme).split()[3]
 if ($CurrPlan -ne $HighPerf) {powercfg /setactive $HighPerf}
 
-
+# Test
