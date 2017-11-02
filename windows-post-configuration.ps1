b17236bc240e10c8a694b0e50fc59e8229dbdf87
Kakkerlakkie <33314920+Kakkerlakkie@users.noreply.github.com>
2017-11-02 15:24:32 +0100

    Opmaak

--- windows-post-configuration.ps1
+++ windows-post-configuration.ps1
@@ -81,4 +81,3 @@ Set-NetFirewallRule -Name "FPS-ICMP4-ERQ-In" -enabled True -Profile "Domain,Priv
 $HighPerf = powercfg /l | %{if($_.contains("High performance")) {$_.split()[3]}}
 $CurrPlan = $(powercfg /getactivescheme).split()[3]
 if ($CurrPlan -ne $HighPerf) {powercfg /setactive $HighPerf}
-
